b109cae1d34c52415e401ccc78a0f5fd844bea31
markedwardmurray <markedwardmurray@gmail.com>
2015-07-12 00:53:17 -0400

    ai player, changed symbols

--- triple-tac.py
+++ triple-tac.py
@@ -1,4 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
 # A 3-D tic-tac-toe game by Mark Edward Murray
+
+import random
+
+
+circle = "◉"
+star = "▼"
 
 # cube and cube-display functions
 
@@ -73,39 +82,86 @@ def display_cube_faces(cube):
 def ask_to_play():
   answer = raw_input("Shall we play a game? (y/n) ")
   if answer[:1].lower() == "y":
-    new_game()
+    players = ask_number_of_players()
+    if players == "2":
+      new_game()
+    elif players == "1":
+      new_ai_game()
   else:
     print "Goodbye."
 
-def new_game():
+def ask_number_of_players():
+  players = raw_input("How many players? (1-2) ")
+  if players == "1" or players == "2":
+    return players
+  else:
+    return ask_number_of_players()
+
+
+def new_ai_game():
   cube = new_3x3x3_cube()
-  turn = "#"
-  display_cube_layers(cube)
+  turn = circle
+  # display_cube_layers(cube)
   display_cube_faces(cube)
   for counter in range(26):
-    if turn == "#":
+    if turn == circle:
       human_turn(turn, cube)
-    elif turn == "~":
+    elif turn == star:
+      ai_turn(turn, cube)
+    score = tally_score(cube)
+    display_score(score)
+    if score["attainable"] == 0:
+      break
+    turn = switch_turns(turn)
+  display_winner(score)
+  ask_to_play()
+
+
+def new_game():
+  cube = new_3x3x3_cube()
+  turn = circle
+  # display_cube_layers(cube)
+  display_cube_faces(cube)
+  for counter in range(26):
+    if turn == circle:
+      human_turn(turn, cube)
+    elif turn == star:
       human_turn(turn, cube)
     score = tally_score(cube)
-    display_score(score)          # <-- will resume here, check win
+    display_score(score)
+    if score["attainable"] == 0:
+      break
     turn = switch_turns(turn)
-  print "The game is over."
+  display_winner(score)
   ask_to_play()
+
+def display_winner(score):
+  octothorpe_lines = score[circle]
+  tilde_lines = score[star]
+  if octothorpe_lines > tilde_lines:
+    print "     %s has won!" % circle
+  elif tilde_lines > octothorpe_lines:
+    print "     %s has won!" % star
+  elif tilde_lines == octothorpe_lines:
+    print "       Draw!"
+
 
 def tally_score(cube):
   victory_lines = collect_victory_lines(cube)
   #display_victory_lines(victory_lines)         #dev line
-  victory_octothorpe = [ "#" , "#" , "#" ]
-  victory_tilde =      [ "~" , "~" , "~" ]
+  victory_octothorpe = [ circle , circle , circle ]
+  victory_tilde =      [ star , star , star ]
   octothorpe_lines = 0
   tilde_lines      = 0
+  attainable_lines = 0
   for line in victory_lines:
     if line == victory_octothorpe:
       octothorpe_lines += 1
     elif line == victory_tilde:
       tilde_lines      += 1
-  return { "#":octothorpe_lines, "~":tilde_lines }
+    elif circle not in line or star not in line:
+      attainable_lines += 1
+  return { circle:octothorpe_lines, star:tilde_lines, "attainable":attainable_lines }
 
 def display_victory_lines(victory_lines): #dev function
   counter = 0
@@ -197,15 +253,15 @@ def organize_cube_faces(cube):
   return [ back, left, top, right, front, bottom ]
 
 def display_score(score):
-  print "       SCORE"
-  print "   # : %d | ~ : %d" % ( score["#"] , score["~"] )
+  print "   %s   SCORE   %s" % ( circle , star )
+  print "   %d   [ %2d]   %d" % ( score[circle] , score["attainable"], score[star] )
   print " "
 
 def switch_turns(turn):
-  if turn == "#":
-    turn = "~"
-  elif turn == "~":
-    turn = "#"
+  if turn == circle:
+    turn = star
+  elif turn == star:
+    turn = circle
   return turn
 
 # human turn functions
@@ -231,7 +287,7 @@ def ask_valid_selection(turn, cube):
      return ask_valid_selection(turn, cube)
 
 def retrieve_valid_moves(cube):
-  invalidating_symbols = [ "-", "#", "~" ]
+  invalidating_symbols = [ "-", circle, star ]
   valid_moves = []
   for layer in cube:
     for row in layer:
@@ -239,6 +295,105 @@ def retrieve_valid_moves(cube):
         if box not in invalidating_symbols:
           valid_moves.append(box)
   return valid_moves
+
+# ai turn functions
+
+def ai_turn(turn, cube):
+  selection = determine_selection(turn, cube)
+  for layer in cube:
+    for row in layer:
+      if selection in row:
+        index = row.index(selection)
+        row[index] = turn
+        break
+  display_cube_faces(cube)
+
+def determine_selection(turn, cube):
+  offense_values = analyze_offense(turn, cube)
+  defense_values = analyze_defense(turn, cube)
+  combined_values = {}
+  for move in offense_values:
+    value = offense_values[move] + defense_values[move]
+    combined_values[move] = value
+    print "%s - %.2f + %.2f = %.2f" % ( move , offense_values[move] , defense_values[move] , combined_values[move] )
+  highest_value = 0.0
+  for move in combined_values:
+    if combined_values[move] > highest_value:
+      highest_value = combined_values[move]
+  print "highest_value = %.2f" % highest_value
+  best_moves = []
+  for move in combined_values:
+    if combined_values[move] == highest_value:
+      best_moves.append(move)
+  print best_moves
+  selection = random.choice(best_moves)
+  print selection
+  return selection
+
+
+def determine_enemy(turn):
+  if turn == circle:
+    return star
+  elif turn == star:
+    return circle
+
+
+def analyze_defense(turn, cube):
+  enemy = determine_enemy(turn)
+  valid_moves = retrieve_valid_moves(cube)
+  defense_values = {}
+  for move in valid_moves:
+    defense_values[move] = 0.0
+  #
+  victory_lines = collect_victory_lines(cube)
+  #
+  for key in defense_values:
+    for line in victory_lines:
+      if key in line:
+        enemy_count = line.count(enemy)
+        self_count = line.count(turn)
+
+        if enemy in line:
+          if turn in line:
+            defense_values[key] += 0.0
+          elif enemy_count == 2:
+            defense_values[key] += 0.98
+          elif enemy_count == 1:
+            defense_values[key] += 0.49
+        elif turn not in line:
+          defense_values[key] += 0.33
+
+  return defense_values
+
+
+def analyze_offense(turn, cube):
+  enemy = determine_enemy(turn)
+  valid_moves = retrieve_valid_moves(cube)
+  offense_values = {}
+  for move in valid_moves:
+    offense_values[move] = 0.0
+  #
+  victory_lines = collect_victory_lines(cube)
+  #
+  for key in offense_values:
+    for line in victory_lines:
+      if key in line:
+        enemy_count = line.count(enemy)
+        self_count = line.count(turn)
+
+        if enemy in line:
+          offense_values[key] += 0.0
+        elif turn in line:
+          if self_count == 2:
+            offense_values[key] += 0.99
+          elif self_count == 1:
+            offense_values[key] += 0.49
+        else:
+            offense_values[key] += 0.33
+  #
+  return offense_values
+
+
 
 # main function call
 ask_to_play()
